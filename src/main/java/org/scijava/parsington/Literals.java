169d5af598d7850372284f8b1bfb74bbe0f3c6d0
Jared Davis <sunray@davisprogramming.com>
2026-06-12 17:50:39 -0400

    Make group 4 of DECIMAL optional

--- src/main/java/org/scijava/parsington/Literals.java
+++ src/main/java/org/scijava/parsington/Literals.java
@@ -55,7 +55,7 @@ public final class Literals {
 		"(([-+]?)0([0-7]+)([Ll]?)).*");
 
 	private static final Pattern DECIMAL = Pattern.compile(
-		"(([-+]?[0-9]+(\\.[0-9]*)?([Ee][-+]?[0-9]+)?)([DdFfLl])?).*");
+		"(([-+]?[0-9]+(\\.[0-9]*)?(?:[Ee][-+]?[0-9]+)?)([DdFfLl])?).*");
 
 	private static final ThreadLocal<Matcher> hexMatcher = ThreadLocal.withInitial(() -> HEX.matcher(""));
 	private static final ThreadLocal<Matcher> binaryMatcher = ThreadLocal.withInitial(() -> BINARY.matcher(""));
@@ -408,7 +408,7 @@ public final class Literals {
 		final Matcher m = decimalMatcher.get().reset(sub(s, pos)); //matcher(DECIMAL, s, pos);
 		if (!m.matches()) return null;
 		final String number = m.group(2);
-		final String force = m.group(5);
+		final String force = m.group(4);
 		final boolean forceLong = "l".equalsIgnoreCase(force);
 		final boolean forceFloat = "f".equalsIgnoreCase(force);
 		final boolean forceDouble = "d".equalsIgnoreCase(force);
