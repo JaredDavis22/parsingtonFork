99c2afa25fb96a92ca16b38e19dc5dd0a8588ddb
Jared Davis <sunray@davisprogramming.com>
2026-07-28 09:50:04 -0400

    Java 8 updates

--- src/main/java/org/scijava/parsington/ExpressionParser.java
+++ src/main/java/org/scijava/parsington/ExpressionParser.java
@@ -151,15 +151,15 @@ public class ExpressionParser {
 
 		// NB: Ensure operators with longer symbols come first.
 		// This prevents e.g. '-' from being matched before '-=' and '--'.
-		Collections.sort(operatorsList, (o1, o2) -> {
-			final String t1 = o1.getToken();
-			final String t2 = o2.getToken();
-			final int len1 = t1.length();
-			final int len2 = t2.length();
-			if (len1 > len2) return -1; // o1 is longer, so o1 comes first.
-			if (len1 < len2) return 1; // o2 is longer, so o2 comes first.
-			return t1.compareTo(t2);
-		});
+		operatorsList.sort((o1, o2) -> {
+            final String t1 = o1.getToken();
+            final String t2 = o2.getToken();
+            final int len1 = t1.length();
+            final int len2 = t2.length();
+            if (len1 > len2) return -1; // o1 is longer, so o1 comes first.
+            if (len1 < len2) return 1; // o2 is longer, so o2 comes first.
+            return t1.compareTo(t2);
+        });
 
 		this.operators = Collections.unmodifiableList(operatorsList);
 		this.elementSeparator = elementSeparator;
